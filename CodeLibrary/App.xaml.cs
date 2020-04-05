804b7e9f03990ec851f9b13661ea805b29c2231c
Cl <cloudsnake@163.com>
2020-04-05 16:27:46 +0800

    add

---
+++ CodeLibrary/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Ioc;
+using CodeLibrary.Views;
+using System.Windows;
+
+namespace CodeLibrary
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
